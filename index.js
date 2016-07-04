fa04607293a57e289f8be00cabea7ac3f9b133a8
Apurv <apurv@capiot.com>
2016-07-04 15:44:05 +0530

    date and random numbers exposed

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 var StateEngine = require("./StateEngine");
+var rand = function (_i) {
+    var i = Math.pow(10, _i - 1);
+    var j = Math.pow(10, _i) - 1;
+    return ((Math.floor(Math.random() * (j - i + 1)) + i));
+};
+var date = function (_dM) {
+    var d = new Date();
+    return new Date(d.setMonth(d.getMonth() - _dM));
+};
 module.exports = {
-    StateEngine: StateEngine
+    StateEngine: StateEngine,
+    rand:   rand,
+    date:   date
 };
