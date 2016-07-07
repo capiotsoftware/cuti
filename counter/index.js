ca59b6447f5499de88fd2bc2809c70bf48b54b32
Apurv <apurv@capiot.com>
2016-07-07 14:31:12 +0530

    Counter added

---
+++ counter/index.js
@@ -0,0 +1,36 @@
+var mongoose = require("mongoose");
+
+
+var date = process.env.EXPIRE?process.env.EXPIRE:new Date("3000-12-31");
+var counterSchema = new mongoose.Schema({
+    _id: {type:   String},
+    next: {type: Number},
+    expiresAt: { type: Date,default:date }     
+});
+counterSchema.index({ expiresAt: 1 }, { expireAfterSeconds : 0 });
+var counterModel = mongoose.model("counter",counterSchema);    
+var setDefaults = function(sequenceName,defaultValue){
+    if(!sequenceName){
+        return;
+    }
+    if(!defaultValue){
+        defaultValue =0;
+    }
+    var options = {};
+    options.new = true;
+    options.upsert = true;
+    options.setDefaultsOnInsert = true;
+    counterModel.findByIdAndUpdate(sequenceName,{$set:{next:defaultValue}},options);
+};
+var getCount = function(sequenceName,expire,callback){
+    var options = {};
+    if(!expire){
+        expire = date;
+    }
+    options.new = true;
+    options.upsert = true;
+    options.setDefaultsOnInsert = true;
+    counterModel.findByIdAndUpdate(sequenceName,{ $inc: { next: 1 }, $set:{expiresAt:expire} }, options,callback);
+};
+module.exports.getCount = getCount;
+module.exports.setDefaults = setDefaults;
