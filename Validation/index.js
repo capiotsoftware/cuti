419fb9d96023292b7acbd278346bcee4a83fe5d6
Apurv <apurv@capiot.com>
2016-09-06 15:24:17 +0530

    Validations made strict

--- Validation/index.js
+++ Validation/index.js
@@ -9,21 +9,30 @@ var init = (_m,_crudder) => {
 var validationGet = (req,res,next) => {
     var select = req.query.select;
     var options = {};
-    options.hostname = req.headers.validationurl.split("//")[1].split(":")[0];
-    options.port = req.headers.validationurl.split(":")[2].split("/")[0];
-    options.path = "/permissionsGet";
-    options.method = "POST";
-    options.headers = {};
-    options.headers = req.headers;
-    options.headers["mastername"] = masterName;
-    http.request(options,function(res){
-        res.on("data",(data) => {
-            data = JSON.parse(data);
-            var newSelect = select?_.intersection(select.split(","),data):data;
-            req.query.select = newSelect.length>0?newSelect.join():"_id";
-            next();    
-        });
-    }).end(); 
+    if(req.headers["validation-url"]){
+        options.hostname = req.headers["validation-url"].split("//")[1].split(":")[0];
+        options.port = req.headers["validation-url"].split(":")[2].split("/")[0];
+        options.path = "/permissionsGet";
+        options.method = "POST";
+        options.headers = {};
+        options.headers = req.headers;
+        options.headers["mastername"] = masterName;
+        http.request(options,function(res){
+            res.on("data",(data) => {
+                data = JSON.parse(data);
+                req.user = data.user;
+                data = data.permission;
+                console.log(data);
+                var newSelect = select?_.intersection(select.split(","),data):data;
+                req.query.select = newSelect.length>0?newSelect.join():"_id";
+                next();    
+            });
+        }).end();
+    }
+    else{
+        req.query.select = "_id";
+        next();
+    } 
 };
 var getListOfVarriables = (obj) =>{
     var list = [];
@@ -35,36 +44,43 @@ var getListOfVarriables = (obj) =>{
 };
 var validationPost = (req,res,next) =>{
     var options = {};
-    options.hostname = req.headers.validationurl.split("//")[1].split(":")[0];
-    options.port = req.headers.validationurl.split(":")[2].split("/")[0];
-    options.path = "/permissionsPost";
-    options.method = "POST";
-    options.headers = req.headers;
-    options.headers["content-length"] = 0;
-    options.headers["mastername"] = masterName;
-    http.request(options,function(result){
-        result.on("data",(permissionData) => {
-            permissionData = permissionData.toString("utf8");
-            permissionData = JSON.parse(permissionData);
-            var flag = permissionData.reduce((prev,curr) =>{
-                var key = Object.keys(curr)[0];
-                var value = getValue(req.body,key);
-                if(curr[key] == false){
-                    return (!value)?prev:false;
-                }
-                else if(curr[key]!=true){
-                    return (curr[key].min<=value && curr[key].max>=value)?prev:false;    
-                }
-                else if(curr[key].type == "%"){
-                    return prev;
-                }
-                else{
-                    return prev;
-                }
-            },true); 
-            flag?next():next(new Error("Create permissions denied")); 
-        });
-    }).end();   
+    if(req.headers["validation-url"]){
+        options.hostname = req.headers["validation-url"].split("//")[1].split(":")[0];
+        options.port = req.headers["validation-url"].split(":")[2].split("/")[0];
+        options.path = "/permissionsPost";
+        options.method = "POST";
+        options.headers = req.headers;
+        options.headers["content-length"] = 0;
+        options.headers["mastername"] = masterName;
+        http.request(options,function(result){
+            result.on("data",(permissionData) => {
+                permissionData = permissionData.toString("utf8");
+                permissionData = JSON.parse(permissionData);
+                req.user = permissionData.user;
+                permissionData = permissionData.permission;
+                var flag = permissionData.reduce((prev,curr) =>{
+                    var key = Object.keys(curr)[0];
+                    var value = getValue(req.body,key);
+                    if(curr[key] == false){
+                        return (!value)?prev:false;
+                    }
+                    else if(curr[key]!=true){
+                        return (curr[key].min<=value && curr[key].max>=value)?prev:false    
+                    }
+                    else if(curr[key].type == "%"){
+                        return prev;
+                    }
+                    else{
+                        return prev;
+                    }
+                },true); 
+                flag?next():next(new Error("Create permissions denied")); 
+            });
+        }).end();
+    }
+    else{
+        res.status(500).json("Validation Url Required");
+    }   
 };
 var getDiff = function(el,oldObj,newObj){
     var diffObj = {};
@@ -98,7 +114,7 @@ var getDiff = function(el,oldObj,newObj){
         }
         else{
             diffObj[el] = {};
-            diffObj[el]["__diff"] = "+";
+            diffObj[el]["__diff"] = "+"
             diffObj[el]["r"] = newObj[el];
         }
     }    
@@ -122,54 +138,68 @@ var getValue = function(obj,key){
     return ans;    
 };
 var validationPut = (req,res,next) =>{
-    var options = {};
-    options.hostname = req.headers.validationurl.split("//")[1].split(":")[0];
-    options.port = req.headers.validationurl.split(":")[2].split("/")[0];
-    options.path = "/permissionsPost";
-    options.method = "POST";
-    options.headers = {};
-    options.headers = req.headers;
-    options.headers["content-length"] = 0;
-    options.headers["mastername"] = masterName;
-    //console.log("inside method", options);
-    http.request(options,function(response){
-        response.on("data",function(permissionData){
-            crudder.model.find({_id:req.body._id},function(err,doc){
-                if(doc.length!=1){
-                    return next(new Error("Invalid object"));
-                }
-                var result = diff(doc[0].toObject(),req.body);            
-                permissionData = permissionData.toString("utf8");
-                permissionData = JSON.parse(permissionData);
-                var flag = permissionData.reduce((prev,curr) =>{
-                    var segment = {};
-                    var key = Object.keys(curr)[0];
-                    segment[key] = getValue(result,key);
-                    if(!segment[key]){
-                        return prev;
+    if(req.headers["validation-url"]){
+        var options = {};
+        options.hostname = req.headers["validation-url"].split("//")[1].split(":")[0];
+        options.port = req.headers["validation-url"].split(":")[2].split("/")[0];
+        options.path = "/permissionsPost";
+        options.method = "POST";
+        options.headers = {};
+        options.headers = req.headers;
+        options.headers["content-length"] = 0;
+        options.headers["mastername"] = masterName;
+        http.request(options,function(response){
+            response.on("data",function(permissionData){
+                crudder.model.find({_id:req.body._id},function(err,doc){
+                    if(doc.length!=1){
+                        return next(new Error("Invalid object"));
                     }
-                    else if(curr[key] == false){
-                        return (!segment[key].r)?prev:false;
-                    }
-                    else if(curr[key]!=true){
-                        if(curr[key].type == "%"){
-                            var val = segment[key].l;
-                            var upperLim = (val+(val*curr[key].max)/100);
-                            var lowerLim = (val+(val*curr[key].min)/100); 
-                            return (segment[key].r>=lowerLim && segment[key].r<=upperLim)?prev:false;
+                    var result = diff(doc[0].toObject(),req.body);            
+                    permissionData = permissionData.toString("utf8");
+                    permissionData = JSON.parse(permissionData);
+                    req.user = permissionData.user;
+                    permissionData = permissionData.permission;
+                    console.log(permissionData,"permissionData");
+                    var flag = permissionData.reduce((prev,curr) =>{
+                        var segment = {};
+                        var key = Object.keys(curr)[0];
+                        console.log("crash happens here",key,curr);
+                        segment[key] = getValue(result,key);
+                        if(!segment[key]){
+                            return prev;
+                        }
+                        else if(!segment[key].r){
+                            return prev;
+                        }
+                        else if(curr[key] == false){
+                            return (!segment[key].r)?prev:false;
+                        }
+                        else if(curr[key]!=true){
+                            if(!segment[key].r){
+                                return prev;
+                            }
+                            else if(curr[key].type == "%"){
+                                var val = segment[key].l;
+                                var upperLim = (val+(val*curr[key].max)/100);
+                                var lowerLim = (val+(val*curr[key].min)/100); 
+                                return (segment[key].r>=lowerLim && segment[key].r<=upperLim)?prev:false;
+                            }
+                            else{
+                                return (curr[key].min<=segment[key].r && curr[key].max>=segment[key].r)?prev:false    
+                            }
                         }
                         else{
-                            return (curr[key].min<=segment[key].r && curr[key].max>=segment[key].r)?prev:false    
+                            return prev;
                         }
-                    }
-                    else{
-                        return prev;
-                    }
-                },true); 
-                flag?next():next(new Error("Write permission to the mentioned fields denied"));    
+                    },true); 
+                    flag?next():next(new Error("Write permission to the mentioned fields denied"));    
+                });
             });
-        });
-    }).end();        
+        }).end();
+    }
+    else{
+        res.status(500).json("Validation Url Required");    
+    }        
 };
 module.exports.validationGet = validationGet;
 module.exports.validationPost = validationPost;
