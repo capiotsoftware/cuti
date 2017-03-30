343f91bd49af0b3e5524b9011411b5df12f62260
Apurv <apurv@capiot.com>
2017-03-30 15:37:59 +0530

    post and get is done'

--- rbac/index.js
+++ rbac/index.js
@@ -1,64 +1,145 @@
 var http = require("http");
+var puttu = require("puttu-redis");
+var _ = require("lodash");
 var request = require("../Request");
 function validateGetRequest(collectionName){
     return function(req,res,next){
-        request.getUrlandMagicKey("user")
-        .then(options => {
-            options.path += "/permissionsGet";
-            options.headers = req.headers;
-            options.headers["content-length"] = 0;
-            options.headers["mastername"] = collectionName;
-            var httpRequest = http.request(options, response => {
-                var data = "";
-                if(response.statusCode == 200){
-                    response.on("data", _data => data += _data.toString());
-                    response.on("end", () => {
-                        try {
-                            data = JSON.parse(data);
-                            var userGroups = data.user.groups;
-                            var allowedFields = [];
-                            if(req.query.select){
-                                req.query.select.split(",").forEach(el => {
-                                    userGroups.every(_el => {
-                                        if(data.permission[el] && data.permission[el]["permissions"][_el] && data.permission[el]["permissions"][_el]!="N"){
-                                            allowedFields.push(el);
-                                            return false;
-                                        }
-                                        return true;
+        if(req.headers["authorization"]){
+            request.getUrlandMagicKey("user")
+            .then(options => {
+                options.path += "/permissionsGet";
+                options.headers = req.headers;
+                options.headers["content-length"] = 0;
+                options.headers["mastername"] = collectionName;
+                var httpRequest = http.request(options, response => {
+                    var data = "";
+                    if(response.statusCode == 200){
+                        response.on("data", _data => data += _data.toString());
+                        response.on("end", () => {
+                            try {
+                                data = JSON.parse(data);
+                                var userGroups = data.user.groups;
+                                var allowedFields = [];
+                                if(req.query.select){
+                                    req.query.select.split(",").forEach(el => {
+                                        userGroups.every(_el => {
+                                            if(data.permission[el] && data.permission[el]["permissions"][_el] && data.permission[el]["permissions"][_el]!="N"){
+                                                allowedFields.push(el);
+                                                return false;
+                                            }
+                                            return true;
+                                        });
                                     });
-                                });
-                            }
-                            else{
-                                console.log(userGroups);
-                                Object.keys(data.permission).forEach(el => {
-                                    userGroups.every(_el => {
-                                        console.log(_el,data.permission[el]);
-                                        if(data.permission[el] && data.permission[el]["permissions"][_el] && data.permission[el]["permissions"][_el]!="N"){
-                                            allowedFields.push(el);
-                                            return false;
-                                        }
-                                        return true;
+                                }
+                                else{
+                                    Object.keys(data.permission).forEach(el => {
+                                        userGroups.every(_el => {
+                                            if(data.permission[el] && data.permission[el]["permissions"][_el] && data.permission[el]["permissions"][_el]!="N"){
+                                                allowedFields.push(el);
+                                                return false;
+                                            }
+                                            return true;
+                                        });
                                     });
-                                });
+                                }
+                                if(allowedFields.length==0)
+                                    allowedFields.push("_id");
+                                req.query.select = allowedFields.join();
+                                next();
                             }
-                            if(allowedFields.length==0)
-                                allowedFields.push("_id");
-                            req.query.select = allowedFields.join();
-                            next();
-                        }
-                        catch(err){
-                            res.status(500).json(err);
-                        }
-                    });
-                }
-                else{
-                    res.status(401).json({message:"Authentication Failed"});
-                }
-            });
-            httpRequest.end();
-            httpRequest.on("error", err => res.status(500).json(err));
-        }).catch(err => res.status(500).json(err));
+                            catch(err){
+                                res.status(500).json(err);
+                            }
+                        });
+                    }
+                    else{
+                        res.status(401).json({message:"Authentication Failed"});
+                    }
+                });
+                httpRequest.end();
+                httpRequest.on("error", err => res.status(500).json(err));
+            }).catch(err => res.status(500).json(err));
+        }
+        else{
+            puttu.getMagicKey("category")
+            .then(key=> key==req.headers.magickey?next():res.status(401).json({message:"Unauthorized"}));
+        }
     }
 }
 
+function validatePostRequest(collectionName){
+    return function(req,res,next){
+        if(req.headers["authorization"]){
+            request.getUrlandMagicKey("user")
+            .then(options => {
+                options.path += "/permissionsGet";
+                options.headers = req.headers;
+                options.headers["content-length"] = 0;
+                options.headers["mastername"] = collectionName;
+                var httpRequest = http.request(options, response => {
+                    var data = "";
+                    if(response.statusCode == 200){
+                        response.on("data", _data => data += _data.toString());
+                        response.on("end", () => {
+                            try {
+                                data = JSON.parse(data);
+                                var userGroups = data.user.groups;
+                                var body = req.body;
+                                var flag = true;
+                                var field = null;
+                                Object.keys(body).every(el => {
+                                    var tempFlag = false;
+                                    userGroups.every(_el => {
+                                        if(data.permission[el] && data.permission[el]["permissions"][_el] && data.permission[el]["permissions"][_el]!="N" && data.permission[el]["permissions"][_el]!="R"){
+                                            if(data.permission[el]["permissions"][_el].indexOf("to")>-1){ 
+                                                var min = parseInt(data.permission[el]["permissions"][_el].split("to")[0]);
+                                                var max = parseInt(data.permission[el]["permissions"][_el].split("to")[1]);
+                                                if(min<=body[el] && max>=body[el]){
+                                                    tempFlag = true;
+                                                    return false;
+                                                }
+                                                else
+                                                    return true;
+                                            }
+                                            else{
+                                                tempFlag = true;
+                                                return false;
+                                            }
+                                        }
+                                        return true;
+                                    });
+                                    flag = flag && tempFlag;
+                                    if(!flag){
+                                        field = el;
+                                    }
+                                    return tempFlag;
+                                });
+                                if(flag){
+                                    next();
+                                }
+                                else{
+                                    res.status(401).json({message:"Write permissions on certain fields is not allowed:- "+field});
+                                }
+                            }
+                            catch(err){
+                                res.status(500).json(err);
+                            }
+                        });
+                    }
+                    else{
+                        res.status(401).json({message:"Authentication Failed"});
+                    }
+                });
+                httpRequest.end();
+                httpRequest.on("error", err => res.status(500).json(err));
+            });     
+        }
+        else{
+            puttu.getMagicKey("category")
+            .then(key=> key==req.headers.magickey?next():res.status(401).json({message:"Unauthorized"}));
+        }
+    }
+}
+
+module.exports.validatePostRequest = validatePostRequest;
 module.exports.validateGetRequest = validateGetRequest;
