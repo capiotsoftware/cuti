2dfde2bdd164616cc58e6f7cb7c9724959957a14
Jerry M <developer.jerrymannel@gmail.com>
2018-05-30 11:09:42 +0530

    logger added debug

--- logger/index.js
+++ logger/index.js
@@ -1,11 +1,12 @@
 var log4js = require("log4js");
 log4js.configure({
     levels: {
+      DEBUG: { value: Number.MAX_VALUE-2, colour: 'grey' },
       OFF: { value: Number.MAX_VALUE-1, colour: 'white' },
       AUDIT: { value: Number.MAX_VALUE, colour: 'yellow' }
     },
     appenders: { out: { type: 'stdout' } },
     categories: { default: { appenders: ['out'], level: 'AUDIT' } }
   });
-log4js.level = process.env.LOG_LEVEL ? process.env.LOG_LEVEL: 'debug';
+log4js.level = process.env.LOG_LEVEL ? process.env.LOG_LEVEL: 'audit';
 module.exports.getLogger = log4js;
